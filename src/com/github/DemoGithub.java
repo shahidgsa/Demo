f83fda60afaf37bf609540d56a93d1968148d3e4
shahidgsa <shahidgsa7@gmail.com>
2024-11-27 16:42:33 +0530

    update code here

--- src/com/github/DemoGithub.java
+++ src/com/github/DemoGithub.java
@@ -2,4 +2,11 @@ package com.github;
 
 public class DemoGithub {
 
+	
+	public static void main(String []args) {
+		
+		
+		System.out.println("code updated successfully");
+		
+	}
 }
--- src/com/github2/DemoGithub2.java
+++ src/com/github2/DemoGithub2.java
@@ -1,5 +1,24 @@
 package com.github2;
 
+
+
 public class DemoGithub2 {
 
+	public static void main(String[]args) {
+		
+		
+		System.out.println("shahid ahmed G");
+		
+		System.out.println("till here code updated");
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
 }
